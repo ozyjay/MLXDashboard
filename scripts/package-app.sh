a5ea6ec1ef518544c1b53b9d162f3851e93c36b8
Jason Holdsworth <jason.holdsworth@jcu.edu.au>
2026-08-24 00:21:56 +1000

    Add application icon handling in package script and update Info.plist

--- scripts/package-app.sh
+++ scripts/package-app.sh
@@ -6,13 +6,30 @@ DIST="$ROOT/dist"
 APP="$DIST/MLXDashboard.app"
 CONTENTS="$APP/Contents"
 MACOS="$CONTENTS/MacOS"
+RESOURCES="$CONTENTS/Resources"
+ICON_SOURCE="$ROOT/Assets/MLXDashboardIcon.png"
+ICONSET="$DIST/MLXDashboard.iconset"
 
 cd "$ROOT"
 swift build -c release --product MLXDashboard
 
 rm -rf "$APP"
-mkdir -p "$MACOS"
+mkdir -p "$MACOS" "$RESOURCES" "$ICONSET"
 cp ".build/release/MLXDashboard" "$MACOS/MLXDashboard"
+
+sips -z 16 16 "$ICON_SOURCE" --out "$ICONSET/icon_16x16.png" >/dev/null
+sips -z 32 32 "$ICON_SOURCE" --out "$ICONSET/icon_16x16@2x.png" >/dev/null
+sips -z 32 32 "$ICON_SOURCE" --out "$ICONSET/icon_32x32.png" >/dev/null
+sips -z 64 64 "$ICON_SOURCE" --out "$ICONSET/icon_32x32@2x.png" >/dev/null
+sips -z 128 128 "$ICON_SOURCE" --out "$ICONSET/icon_128x128.png" >/dev/null
+sips -z 256 256 "$ICON_SOURCE" --out "$ICONSET/icon_128x128@2x.png" >/dev/null
+sips -z 256 256 "$ICON_SOURCE" --out "$ICONSET/icon_256x256.png" >/dev/null
+sips -z 512 512 "$ICON_SOURCE" --out "$ICONSET/icon_256x256@2x.png" >/dev/null
+sips -z 512 512 "$ICON_SOURCE" --out "$ICONSET/icon_512x512.png" >/dev/null
+sips -z 1024 1024 "$ICON_SOURCE" --out "$ICONSET/icon_512x512@2x.png" >/dev/null
+iconutil -c icns "$ICONSET" -o "$RESOURCES/MLXDashboard.icns"
+rm -rf "$ICONSET"
+
 cat > "$CONTENTS/Info.plist" <<'PLIST'
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
@@ -24,6 +41,8 @@ cat > "$CONTENTS/Info.plist" <<'PLIST'
   <string>com.local.MLXDashboard</string>
   <key>CFBundleName</key>
   <string>MLXDashboard</string>
+  <key>CFBundleIconFile</key>
+  <string>MLXDashboard.icns</string>
   <key>CFBundlePackageType</key>
   <string>APPL</string>
   <key>CFBundleShortVersionString</key>
